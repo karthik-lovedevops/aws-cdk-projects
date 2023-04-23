1c40e5b104bb6588fe6d1480b872f7a0a2715d92
karthik <karthikdevopscloud1984@gmail.com>
2023-04-23 15:36:20 +0400

    lambda function deploy using cdk

--- cdk_project/cdk_project_stack.py
+++ cdk_project/cdk_project_stack.py
@@ -1,11 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_lambda as _lambda,
 )
 
 
@@ -13,6 +9,13 @@ class CdkProjectStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime = _lambda.Runtime.PYTHON_3_9,
+            code = _lambda.Code.from_asset('lambda'),
+            handler='hello.handler'
+        )
 
         # queue = sqs.Queue(
         #     self, "CdkProjectQueue",
