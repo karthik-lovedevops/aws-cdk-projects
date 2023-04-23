4a4bfd985d78ef1c895616f20e75951c4c6073cd
karthik <karthikdevopscloud1984@gmail.com>
2023-04-23 16:24:35 +0400

    add resources to the hits counter contruct

--- cdk_project/hitcounter.py
+++ cdk_project/hitcounter.py
@@ -1,6 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
     aws_lambda as _lambda,
+    aws_dynamodb as ddb,
 )
 
 class HitCounter(Construct):
@@ -8,4 +9,25 @@ class HitCounter(Construct):
     def __init__(self, scope: Construct, id: str, downstream: _lambda.IFunction, **kwargs):
         super().__init__(scope, id, **kwargs)
 
-        # TODO
+    @property
+    def handler(self):
+        return self._handler    
+
+    def __init__(self, scope: Construct, id: str, downstream: _lambda.IFunction, **kwargs):
+        super().__init__(scope, id, **kwargs)
+
+        table = ddb.Table(
+            self, 'Hits',
+            partition_key={'name': 'path', 'type': ddb.AttributeType.STRING}
+        )
+
+        self._handler = _lambda.Function(
+            self, 'HitCountHandler',
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            handler='hitcount.handler',
+            code=_lambda.Code.from_asset('lambda'),
+            environment={
+                'DOWNSTREAM_FUNCTION_NAME': downstream.function_name,
+                'HITS_TABLE_NAME': table.table_name,
+            }
+        )
